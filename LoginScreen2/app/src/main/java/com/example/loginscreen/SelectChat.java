cf4a8e86ac7d712f4529caf1af14337381a21d99
YousefShahin10 <122842934+YousefShahin10@users.noreply.github.com>
2024-04-07 18:13:50 -0400

    ui changes and authorization for registration

--- LoginScreen2/app/src/main/java/com/example/loginscreen/SelectChat.java
+++ LoginScreen2/app/src/main/java/com/example/loginscreen/SelectChat.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,9 +20,18 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.firestore.QuerySnapshot;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,7 +43,9 @@ public class SelectChat extends AppCompatActivity {
 
     private FloatingActionButton searchBtn;
     private String searchEntry;
-
+    Button CreateBtn;
+    FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     private void populateDummyChats() {
@@ -45,10 +58,20 @@ public class SelectChat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_chat);
-
+        CreateBtn = (Button)findViewById(R.id.CreateBtn);
         populateDummyChats();
         searchBtn = findViewById(R.id.SearchBtn);
         EditText searchBar = findViewById(R.id.search);
+
+
+        // Initialize Firebase Auth
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String email = currentUser.getEmail();
+
+
+
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -89,5 +112,26 @@ public class SelectChat extends AppCompatActivity {
 //            startActivity(intent);
 //        });
 //        chatListRecyclerView.setAdapter(adapter);
+        CreateBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //Redirect to Create User page
+                db.collection("Users").document(email).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        String permission = task.getResult().getData().get("Type").toString();
+                        if (permission.equals("Admin")){
+                            Intent intent = new Intent(SelectChat.this, CreateUser.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(SelectChat.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                });
+
+            }
+        });
     }
+
 }
